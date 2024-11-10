f674a1ed966a89214c4538b234545c993d9ad963
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-11-10 14:40:38 +0100

    test(auth): creat new unit tests for code validation

--- app/modules/auth/tests/test_unit.py
+++ app/modules/auth/tests/test_unit.py
@@ -1,8 +1,9 @@
+from datetime import datetime, timedelta, timezone
 import pytest
 from flask import url_for
 
 from app.modules.auth.services import AuthenticationService
-from app.modules.auth.repositories import UserRepository
+from app.modules.auth.repositories import UserRepository, SignUpVerificationTokenRepository
 from app.modules.profile.repositories import UserProfileRepository
 
 
@@ -75,7 +76,7 @@ def test_signup_user_successful(test_client):
     assert response.request.path == url_for("auth.show_signup_form"), "Signup was unsuccessful"
 
 
-def test_service_create_with_profie_success(clean_database):
+def test_service_create_with_profile_success(clean_database):
     data = {
         "name": "Test",
         "surname": "Foo",
@@ -117,3 +118,100 @@ def test_service_create_with_profile_fail_no_password(clean_database):
 
     assert UserRepository().count() == 0
     assert UserProfileRepository().count() == 0
+
+
+def test_signup_code_validation_no_code(test_client):
+    response = test_client.post(
+        "/signup/code-validation",
+        data=dict(code=""),
+        follow_redirects=True
+    )
+    assert response.request.path == url_for("auth.validate_code"), "Signup code validation was unsuccessful"
+    assert b"This field is required" in response.data, response.data
+
+
+def test_signup_code_unsuccessful(test_client):
+    wrong_code = "wrong"
+    response = test_client.post(
+        "/signup/code-validation",
+        data=dict(code=wrong_code),
+        follow_redirects=True,
+    )
+    assert response.request.path == url_for("auth.validate_code"), "Signup code validation was unsuccessful"
+
+
+def test_service_create_code_succesful(clean_database):
+    email = "service_test@example.com"
+
+    AuthenticationService().generate_signup_verification_token(email)
+
+    assert SignUpVerificationTokenRepository().count() == 1
+
+
+def test_service_create_code_user_already_with_code(clean_database):
+    email = "service_test@example.com"
+
+    AuthenticationService().generate_signup_verification_token(email)
+    AuthenticationService().generate_signup_verification_token(email)
+
+    assert SignUpVerificationTokenRepository().count() == 1
+
+
+def test_service_verify_code_no_code(clean_database):
+    email = "service_test@example.com"
+    fake_code = None
+
+    acceptance = AuthenticationService().validate_signup_verification_token(email, fake_code)
+
+    assert SignUpVerificationTokenRepository().count() == 0
+    assert not acceptance
+
+
+def test_service_verify_code_user_with_no_code(clean_database):
+    email = "service_test@example.com"
+    fake_code = "wrong"
+
+    acceptance = AuthenticationService().validate_signup_verification_token(email, fake_code)
+
+    assert SignUpVerificationTokenRepository().count() == 0
+    assert not acceptance
+
+
+def test_service_verify_code_outdated(clean_database):
+    email = "service_test@example.com"
+    MAX_VERIFICATION_TOKEN_AGE = AuthenticationService().get_max_verification_token_age()
+    current_date = datetime.now(timezone.utc)
+    outdated_date = current_date - timedelta(seconds=(MAX_VERIFICATION_TOKEN_AGE+1))
+    data = {
+        "email": email,
+        "token": "whatever",
+        "created_at": outdated_date
+    }
+
+    token_repository = SignUpVerificationTokenRepository()
+    token = token_repository.create(**data)
+    acceptance = AuthenticationService().validate_signup_verification_token(email, token)
+
+    assert SignUpVerificationTokenRepository().count() == 0
+    assert not acceptance
+
+
+def test_service_create_code_wrong_code(clean_database):
+    email = "service_test@example.com"
+    wrong_code = "false_code"
+
+    AuthenticationService().generate_signup_verification_token(email)
+    acceptance = AuthenticationService().validate_signup_verification_token(email, wrong_code)
+
+    assert SignUpVerificationTokenRepository().count() == 1
+    assert not acceptance
+
+
+def test_service_create_code_unsuccesful(clean_database):
+    email = "service_test@example.com"
+
+    true_code = AuthenticationService().generate_signup_verification_token(email)
+    acceptance = AuthenticationService().validate_signup_verification_token(email, true_code)
+
+    assert SignUpVerificationTokenRepository().count() == 0
+    assert acceptance
