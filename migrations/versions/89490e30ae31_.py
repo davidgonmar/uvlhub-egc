6914602965b74db95c5fe57bfecc4b122555d232
Carlos García Martínez <carlos.garcia.mtnez@gmail.com>
2024-11-10 17:33:24 +0100

    feat(auth): add developer backend logic

---
+++ migrations/versions/89490e30ae31_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 89490e30ae31
+Revises: 2cf3c96aa0f4, 986022f81e08
+Create Date: 2024-11-11 19:14:53.005481
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '89490e30ae31'
+down_revision = ('2cf3c96aa0f4', '986022f81e08')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
