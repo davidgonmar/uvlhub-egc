f20d3cf1027573170fe5d5ad38e188b1a5d1aba1
Adriana Vento Conesa <adrvencon@alum.us.es>
2024-11-10 11:30:14 +0100

    feat(auth): create reset password token repository

--- app/modules/auth/repositories.py
+++ app/modules/auth/repositories.py
@@ -1,4 +1,4 @@
-from app.modules.auth.models import User, SignUpVerificationToken
+from app.modules.auth.models import User, SignUpVerificationToken, ResetPasswordVerificationToken
 from core.repositories.BaseRepository import BaseRepository
 
 
@@ -26,6 +26,26 @@ class SignUpVerificationTokenRepository(BaseRepository):
         super().__init__(SignUpVerificationToken)
 
     def create(self, commit: bool = True, **kwargs) -> SignUpVerificationToken:
+        instance = self.model(**kwargs)
+        self.session.add(instance)
+        if commit:
+            self.session.commit()
+        else:
+            self.session.flush()
+        return instance
+
+    def get_by_email(self, email: str):
+        return self.model.query.filter_by(email=email).first()
+
+    def get_by_token(self, token: str):
+        return self.model.query.filter_by(token=token).first()
+    
+    
+class ResetPasswordVerificationTokenRepository(BaseRepository):
+    def __init__(self):
+        super().__init__(ResetPasswordVerificationToken)
+
+    def create(self, commit: bool = True, **kwargs) -> ResetPasswordVerificationToken:
         instance = self.model(**kwargs)
         self.session.add(instance)
         if commit:
