871af51919c31be0562015fba109e306f7b7535d
Raul Heras <93604567+raulherasperez@users.noreply.github.com>
2024-11-11 18:48:43 +0100

    fix(auth): remove unused imports

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import render_template, redirect, url_for, request, session, flash, current_app as app
+
 
 from flask import render_template, redirect, url_for, request, session, current_app, flash
 
