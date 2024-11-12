c8324b7a974f2a399907feae19349895e8bd9221
luciaPG <lucpergut@alum.us.es>
2024-11-12 19:33:24 +0100

    refactor(dataset): changed code structure for codacy

--- app/modules/dataset/tests/test_selenium_2.py
+++ app/modules/dataset/tests/test_selenium_2.py
@@ -12,11 +12,10 @@ class TestUI():
   def test_selenium(self):
     self.driver.get("http://localhost:5000/")
     self.driver.set_window_size(1536, 816)
-    self.driver.find_element(By.LINK_TEXT,"Sample dataset 4").click() 
+    self.driver.find_element(By.LINK_TEXT,"Sample dataset 4").click()
     self.driver.find_element(By.ID, "btnGroupDropExport132").click()
     self.driver.find_element(By.ID, "btnGroupDropExport132").click()
     self.driver.find_element(By.ID, "btnGroupDrop132").click()
     self.driver.find_element(By.LINK_TEXT,"Syntax check").click()
     self.driver.find_element(By.CSS_SELECTOR,".list-group-item:nth-child(2) .col-12 > .btn").click()
     self.driver.close()
-  
