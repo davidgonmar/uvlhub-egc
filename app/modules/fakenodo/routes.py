4b7fc43f3adf3b0922648a51ab5c8714a33f7826
Rafael Castillo Cebolla <rafaelcastillocebolla@gmail.com>
2024-11-15 23:41:07 +0100

    Revert "tests: write load tests for Fakenodo. Closes #93"
    This reverts commit 33064780be642a105111c965f0cbb36c7f6e8670.

--- app/modules/fakenodo/routes.py
+++ app/modules/fakenodo/routes.py
@@ -3,20 +3,17 @@ from app.modules.fakenodo import fakenodo_bp
 
 base_url = "/fakenodo/api"
 
-
 # Test connection (GET)
 @fakenodo_bp.route(base_url, methods=["GET"])
 def test_connection_fakenodo():
     response = {"status": "success", "message": "Connected to FakenodoAPI"}
     return jsonify(response)
 
-
 # Create deposition (POST)
 @fakenodo_bp.route(base_url, methods=["POST"])
 def create_fakenodo():
     response = {"status": "success", "message": "Fakenodo deposition created"}
     return make_response(jsonify(response), 201)
-
 
 # Upload files to deposition (POST)
 @fakenodo_bp.route(base_url + "/<depositionId>/files", methods=["POST"])
@@ -27,7 +24,6 @@ def deposition_files_fakenodo(depositionId):
     }
     return make_response(jsonify(response), 201)
 
-
 # Get deposition (GET)
 @fakenodo_bp.route(base_url + "/<depositionId>", methods=["GET"])
 def get_deposition_fakenodo(depositionId):
@@ -37,7 +33,6 @@ def get_deposition_fakenodo(depositionId):
         "doi": f"10.5072/fakenodo.{depositionId}",
     }
     return make_response(jsonify(response), 200)
-
 
 # Delete deposition (DELETE)
 @fakenodo_bp.route(base_url + "/<depositionId>", methods=["DELETE"])
