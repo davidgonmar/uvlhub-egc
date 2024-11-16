36477ee913a5b12017ee914e86ed0e09f928014d
clameaitu <clameaitu@alum.us.es>
2024-11-16 19:41:02 +0100

    feat(explore): add functionality to the button search

--- app/modules/explore/assets/scripts.js
+++ app/modules/explore/assets/scripts.js
@@ -1,138 +1,104 @@
 document.addEventListener('DOMContentLoaded', () => {
-    send_query();
+    document.getElementById('search-button').addEventListener('click', send_query);
 });
 
+
 function send_query() {
+    console.log("send query...");
 
-    console.log("send query...")
-
+    // Clear previous results and hide "not found" icon
     document.getElementById('results').innerHTML = '';
     document.getElementById("results_not_found").style.display = "none";
-    console.log("hide not found icon");
 
-    const filters = document.querySelectorAll('#filters input, #filters select, #filters [type="radio"]');
+    // Get the filter values
+    const searchCriteria = {
+        csrf_token: document.getElementById('csrf_token').value, // CSRF token
+        query: document.querySelector('#query').value,
+        publication_type: document.querySelector('#publication_type').value,
+        sorting: document.querySelector('[name="sorting"]:checked').value,
+    };
 
-    filters.forEach(filter => {
-        filter.addEventListener('input', () => {
-            const csrfToken = document.getElementById('csrf_token').value;
+    // Debug log
+    console.log("Search Criteria:", searchCriteria);
 
-            const searchCriteria = {
-                csrf_token: csrfToken,
-                query: document.querySelector('#query').value,
-                publication_type: document.querySelector('#publication_type').value,
-                sorting: document.querySelector('[name="sorting"]:checked').value,
-            };
+    // Perform the search request (AJAX request)
+    fetch('/explore', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(searchCriteria),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Log the search results
 
-            console.log(document.querySelector('#publication_type').value);
+        // Clear previous results
+        document.getElementById('results').innerHTML = '';
 
-            fetch('/explore', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(searchCriteria),
-            })
-                .then(response => response.json())
-                .then(data => {
+        // Results counter
+        const resultCount = data.length;
+        const resultText = resultCount === 1 ? 'dataset' : 'datasets';
+        document.getElementById('results_number').textContent = `${resultCount} ${resultText} found`;
 
-                    console.log(data);
-                    document.getElementById('results').innerHTML = '';
+        // Show/hide "not found" icon
+        if (resultCount === 0) {
+            document.getElementById("results_not_found").style.display = "block";
+        } else {
+            document.getElementById("results_not_found").style.display = "none";
+        }
 
-                    // results counter
-                    const resultCount = data.length;
-                    const resultText = resultCount === 1 ? 'dataset' : 'datasets';
-                    document.getElementById('results_number').textContent = `${resultCount} ${resultText} found`;
-
-                    if (resultCount === 0) {
-                        console.log("show not found icon");
-                        document.getElementById("results_not_found").style.display = "block";
-                    } else {
-                        document.getElementById("results_not_found").style.display = "none";
-                    }
-
-
-                    data.forEach(dataset => {
-                        let card = document.createElement('div');
-                        card.className = 'col-12';
-                        card.innerHTML = `
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="d-flex align-items-center justify-content-between">
-                                        <h3><a href="${dataset.url}">${dataset.title}</a></h3>
-                                        <div>
-                                            <span class="badge bg-primary" style="cursor: pointer;" onclick="set_publication_type_as_query('${dataset.publication_type}')">${dataset.publication_type}</span>
-                                        </div>
-                                    </div>
-                                    <p class="text-secondary">${formatDate(dataset.created_at)}</p>
-
-                                    <div class="row mb-2">
-
-                                        <div class="col-md-4 col-12">
-                                            <span class=" text-secondary">
-                                                Description
-                                            </span>
-                                        </div>
-                                        <div class="col-md-8 col-12">
-                                            <p class="card-text">${dataset.description}</p>
-                                        </div>
-
-                                    </div>
-
-                                    <div class="row mb-2">
-
-                                        <div class="col-md-4 col-12">
-                                            <span class=" text-secondary">
-                                                Authors
-                                            </span>
-                                        </div>
-                                        <div class="col-md-8 col-12">
-                                            ${dataset.authors.map(author => `
-                                                <p class="p-0 m-0">${author.name}${author.affiliation ? ` (${author.affiliation})` : ''}${author.orcid ? ` (${author.orcid})` : ''}</p>
-                                            `).join('')}
-                                        </div>
-
-                                    </div>
-
-                                    <div class="row mb-2">
-
-                                        <div class="col-md-4 col-12">
-                                            <span class=" text-secondary">
-                                                Tags
-                                            </span>
-                                        </div>
-                                        <div class="col-md-8 col-12">
-                                            ${dataset.tags.map(tag => `<span class="badge bg-primary me-1" style="cursor: pointer;" onclick="set_tag_as_query('${tag}')">${tag}</span>`).join('')}
-                                        </div>
-
-                                    </div>
-
-                                    <div class="row">
-
-                                        <div class="col-md-4 col-12">
-
-                                        </div>
-                                        <div class="col-md-8 col-12">
-                                            <a href="${dataset.url}" class="btn btn-outline-primary btn-sm" id="search" style="border-radius: 5px;">
-                                                View dataset
-                                            </a>
-                                            <a href="/dataset/download/${dataset.id}" class="btn btn-outline-primary btn-sm" id="search" style="border-radius: 5px;">
-                                                Download (${dataset.total_size_in_human_format})
-                                            </a>
-                                        </div>
-
-
-                                    </div>
-
-                                </div>
+        // Display the results
+        data.forEach(dataset => {
+            let card = document.createElement('div');
+            card.className = 'col-12';
+            card.innerHTML = `
+                <div class="card">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between">
+                            <h3><a href="${dataset.url}">${dataset.title}</a></h3>
+                            <div>
+                                <span class="badge bg-primary" style="cursor: pointer;" onclick="set_publication_type_as_query('${dataset.publication_type}')">${dataset.publication_type}</span>
                             </div>
-                        `;
+                        </div>
+                        <p class="text-secondary">${formatDate(dataset.created_at)}</p>
 
-                        document.getElementById('results').appendChild(card);
-                    });
-                });
+                        <div class="row mb-2">
+                            <div class="col-md-4 col-12"><span class=" text-secondary">Description</span></div>
+                            <div class="col-md-8 col-12"><p class="card-text">${dataset.description}</p></div>
+                        </div>
+
+                        <div class="row mb-2">
+                            <div class="col-md-4 col-12"><span class=" text-secondary">Authors</span></div>
+                            <div class="col-md-8 col-12">
+                                ${dataset.authors.map(author => `
+                                    <p class="p-0 m-0">${author.name}${author.affiliation ? ` (${author.affiliation})` : ''}${author.orcid ? ` (${author.orcid})` : ''}</p>
+                                `).join('')}
+                            </div>
+                        </div>
+
+                        <div class="row mb-2">
+                            <div class="col-md-4 col-12"><span class=" text-secondary">Tags</span></div>
+                            <div class="col-md-8 col-12">
+                                ${dataset.tags.map(tag => `<span class="badge bg-primary me-1" style="cursor: pointer;" onclick="set_tag_as_query('${tag}')">${tag}</span>`).join('')}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4 col-12"></div>
+                            <div class="col-md-8 col-12">
+                                <a href="${dataset.url}" class="btn btn-outline-primary btn-sm" style="border-radius: 5px;">View dataset</a>
+                                <a href="/dataset/download/${dataset.id}" class="btn btn-outline-primary btn-sm" style="border-radius: 5px;">Download (${dataset.total_size_in_human_format})</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+            document.getElementById('results').appendChild(card);
         });
     });
 }
+
 
 function formatDate(dateString) {
     const options = {day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric'};
@@ -161,27 +127,24 @@ function set_publication_type_as_query(publicationType) {
 document.getElementById('clear-filters').addEventListener('click', clearFilters);
 
 function clearFilters() {
-
     // Reset the search query
     let queryInput = document.querySelector('#query');
-    queryInput.value = "";
-    // queryInput.dispatchEvent(new Event('input', {bubbles: true}));
+    queryInput.value = ""; // Reset the query input
 
     // Reset the publication type to its default value
     let publicationTypeSelect = document.querySelector('#publication_type');
-    publicationTypeSelect.value = "any"; // replace "any" with whatever your default value is
-    // publicationTypeSelect.dispatchEvent(new Event('input', {bubbles: true}));
+    publicationTypeSelect.value = "any"; // Default value
 
-    // Reset the sorting option
+    // Reset the sorting option to "newest"
     let sortingOptions = document.querySelectorAll('[name="sorting"]');
     sortingOptions.forEach(option => {
-        option.checked = option.value == "newest"; // replace "default" with whatever your default value is
-        // option.dispatchEvent(new Event('input', {bubbles: true}));
+        option.checked = option.value == "newest"; // Default sorting
     });
 
     // Perform a new search with the reset filters
-    queryInput.dispatchEvent(new Event('input', {bubbles: true}));
+    send_query(); // Trigger the search after clearing filters
 }
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
