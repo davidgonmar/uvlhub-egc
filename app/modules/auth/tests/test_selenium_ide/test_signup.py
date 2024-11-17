cf3743eaa24a0df77f8ce1270e63b279da79eb25
luciaPG <lucpergut@alum.us.es>
2024-11-17 13:41:57 +0100

    test(auth): correct selenium test error
    Corrected a selenium test which gives error in different pcs, now it should work for everyone

--- app/modules/auth/tests/test_selenium_ide/test_signup.py
+++ app/modules/auth/tests/test_selenium_ide/test_signup.py
@@ -15,7 +15,9 @@ class TestSignup():
 
     def test_signup(self):
         self.driver.get(get_host_for_selenium_testing())
-        self.driver.find_element(By.LINK_TEXT, "Sign Up").click()
+        signup_link = self.driver.find_element(By.XPATH, "/html/body/div/div/nav/div/ul/li/a[2]")
+        signup_link.click()
+    
         self.driver.find_element(By.ID, "email").send_keys("user1@example.com")
         self.driver.find_element(By.ID, "password").send_keys("1234")
         self.driver.find_element(By.ID, "name").click()
