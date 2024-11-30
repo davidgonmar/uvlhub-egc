e72a959986f207ee8d9c9174c096d354fc2db7bc
Sergiotg03 <sergiotg003@gmail.com>
2024-11-30 21:18:53 +0100

    feat(explore): add dataset rating frontend in explore screen
    First version of the feature (incomplete)
    
    #128

--- app/modules/explore/routes.py
+++ app/modules/explore/routes.py
@@ -3,14 +3,26 @@ from flask import render_template, request, jsonify
 from app.modules.explore import explore_bp
 from app.modules.explore.forms import ExploreForm
 from app.modules.explore.services import ExploreService
+from app.modules.dataset.services import DSRatingService
 
+ds_rating_service = DSRatingService()
 
 @explore_bp.route('/explore', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         query = request.args.get('query', '')
         form = ExploreForm()
-        return render_template('explore/index.html', form=form, query=query)
+        
+        # Filtrar los datasets
+        datasets = ExploreService().filter(query=query)
+        
+        # Calcular el average_rating para cada dataset (NO FUNCIONA, HAY QUE INTENTAR PASAR EL AVERAGE DE CADA UNO AL HTML O SCRIPT.JS)
+        # Como referencia puedes mirar el de home page (modules/public/templates/index.html) y su route
+        for dataset in datasets:
+            average_rating = ds_rating_service.get_average_by_dataset(dataset.id) or 0.0
+            dataset.average_rating = round(average_rating, 2)  # Añadir el average_rating al dataset
+
+        return render_template('explore/index.html', form=form, query=query, datasets=datasets)
 
     if request.method == 'POST':
         criteria = request.get_json()
