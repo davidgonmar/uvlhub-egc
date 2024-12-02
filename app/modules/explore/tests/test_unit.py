2b23d2be81e3bbf4a8f8c9884307468c2f0cbdc1
Playeira01 <mcarmencd00@gmail.com>
2024-12-02 22:27:13 +0100

    test(explore): add unit test for search query functionality to ensure it does not return more datasets than available
    This commit introduces a new unit test for the search query functionality in the explore module. The test validates that the system does not return more datasets than are available, ensuring accurate query results.
    
    #133

--- app/modules/explore/tests/test_unit.py
+++ app/modules/explore/tests/test_unit.py
@@ -341,4 +341,44 @@ def test_filter_functionality_no_input(mock_filter):
     assert len(response) == 2
     assert response[0].to_dict()["title"] == "Dataset 1"
     assert response[1].to_dict()["title"] == "Dataset 2"
-    mock_filter.assert_called_once_with(query="")   
+    mock_filter.assert_called_once_with(query="")
+    
+
+@patch('app.modules.explore.services.ExploreService.filter')  # Mock the specific method
+def test_filter_functionality_number_dataset_is_not_above_reality(mock_filter):
+    # Create dataset mocks
+    mock_dataset_1 = MagicMock()
+    mock_dataset_1.to_dict.return_value = {
+        "id": 1,
+        "title": "Dataset 1",
+        "description": "Description for dataset 1",
+        "url": "/dataset/1",
+        "authors": [{"name": "Author 1"}],
+        "tags": ["tag1", "tag2"],
+        "created_at": "2023-01-01",
+    }
+
+    mock_dataset_2 = MagicMock()
+    mock_dataset_2.to_dict.return_value = {
+        "id": 2,
+        "title": "Dataset 2",
+        "description": "Description for dataset 2",
+        "url": "/dataset/2",
+        "authors": [{"name": "Author 2"}],
+        "tags": ["tag3"],
+        "created_at": "2023-02-01",
+    }
+    # Configure the mock
+    mock_filter.return_value = [mock_dataset_1, mock_dataset_2]
+
+    # Call the function directly
+    service = ExploreService()
+    response = service.filter(query="")
+
+    # Assertions
+    assert len(response) < 3
+    assert response[0].to_dict()["title"] == "Dataset 1"
+    assert response[1].to_dict()["title"] == "Dataset 2"
+    mock_filter.assert_called_once_with(query="")
+
+ 
