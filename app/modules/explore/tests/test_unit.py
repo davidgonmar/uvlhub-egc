e8976df1afde5047f6ca2aa61a4e5bb4e89d0332
Playeira01 <mcarmencd00@gmail.com>
2024-12-03 10:09:18 +0100

    test(explore): add functional unit test for search query functionality with auth input (happy path)
    This commit introduces a new functional unit test for the search query functionality in the explore module (happy path). The test validates the proper handling of mocked datasets and ensures the expected behavior, specifically when the dataset's auth is included in the input.
    
    #133

--- app/modules/explore/tests/test_unit.py
+++ app/modules/explore/tests/test_unit.py
@@ -886,3 +886,49 @@ def test_filter_functionality__the_route_happy_sad_path_description(mock_filter,
     mock_filter.assert_called_once_with(query="Description for dataset 5", **{})
   
     
+@patch('app.modules.explore.services.ExploreService.filter')  # Mock the specific method
+def test_filter_functionality__the_route_happy_happy_path_author(mock_filter, test_client):
+    # Create dataset mocks
+    mock_dataset_1 = MagicMock()
+    mock_dataset_1.to_dict.return_value = {
+        "id": 1,
+        "title": "Dataset 1",
+        "description": "Description for dataset 1",
+        "url": "/dataset/1",
+        "authors": [{"name": "Author 1"}],
+        "tags": ["tag1", "tag2"],
+        "created_at": "2023-01-01",
+    }
+
+    mock_dataset_2 = MagicMock()
+    mock_dataset_2.to_dict.return_value = {
+        "id": 2,
+        "title": "Dataset 2",
+        "description": "Description for dataset 2",
+        "url": "/dataset/2",
+        "authors": [{"name": "Author 2"}],
+        "tags": ["tag3"],
+        "created_at": "2023-02-01",
+    }
+    # Configure the mock
+    mock_filter.return_value = [ mock_dataset_2]
+
+    # Make a request to the endpoint
+    response = test_client.post(
+        '/explore',
+        json={
+            "query": "Author 2",
+            **{}
+        }
+    )
+
+    # Verificar que la respuesta tiene el estado correcto
+    assert response.status_code == 200
+    
+    # Verify that the response has the correct status
+    data = response.json
+
+    # Assertions
+    assert len(data) == 1
+    assert data[0]["title"] == "Dataset 2"
+    mock_filter.assert_called_once_with(query="Author 2", **{})
