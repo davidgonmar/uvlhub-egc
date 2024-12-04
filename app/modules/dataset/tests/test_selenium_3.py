fa5ca2bbb57aaa3fe734c0a3fcd056ad6454c3ae
JaviGarcia1 <javigarciasebastian@icloud.com>
2024-12-04 16:52:46 +0100

    chore(dataset): fix codacy issues
    #118

--- app/modules/dataset/tests/test_selenium_3.py
+++ app/modules/dataset/tests/test_selenium_3.py
@@ -5,13 +5,15 @@
 # from selenium import webdriver
 from selenium.webdriver.common.by import By
 from core.selenium.common import initialize_driver
+
 # from selenium.webdriver.common.action_chains import ActionChains
 # from selenium.webdriver.support import expected_conditions
 # from selenium.webdriver.support.wait import WebDriverWait
 # from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestTest2():
+
+class TestTest2:
     def setup_method(self, method):
         self.driver = initialize_driver()
         self.vars = {}
@@ -26,9 +28,16 @@ class TestTest2():
         self.driver.find_element(By.ID, "btnGroupDropExport3").click()
         self.driver.find_element(By.ID, "search").click()
         self.driver.find_element(By.ID, "btnGroupDropExport3").click()
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) > .dropdown-item").click()
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(2) > .dropdown-item").click()
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(1) > .dropdown-item").click()
-        self.driver.find_element(By.CSS_SELECTOR, "li:nth-child(3) > .dropdown-item").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "li:nth-child(1) > .dropdown-item"
+        ).click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "li:nth-child(2) > .dropdown-item"
+        ).click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "li:nth-child(1) > .dropdown-item"
+        ).click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, "li:nth-child(3) > .dropdown-item"
+        ).click()
         self.driver.close()
-        
