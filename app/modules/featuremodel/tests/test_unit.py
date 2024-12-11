25059f9352e0a7f1cc3d7c28bc9430e8f05b4ebb
David González Martínez <me@davidgonmar.me>
2024-12-11 19:06:13 +0100

    test(featuremodel): add tests for FMRatingService
    issue: #69

--- app/modules/featuremodel/tests/test_unit.py
+++ app/modules/featuremodel/tests/test_unit.py
@@ -1,24 +1,29 @@
 import pytest
+from unittest.mock import MagicMock, patch
+from app.modules.featuremodel.services import FMRatingService
 
+# ===================================================== UNIT TESTS FOR FMRatingService =====================================================
+@pytest.fixture
+def fm_rating_service():
+    return FMRatingService()
 
-@pytest.fixture(scope='module')
-def test_client(test_client):
-    """
-    Extends the test_client fixture to add additional specific data for module testing.
-    """
-    with test_client.application.app_context():
-        # Add HERE new elements to the database that you want to exist in the test context.
-        # DO NOT FORGET to use db.session.add(<element>) and db.session.commit() to save the data.
-        pass
+def test_get(fm_rating_service):
+    with patch('app.modules.featuremodel.services.FMRatingRepository.get') as mock_get:
+        ret = MagicMock()
+        mock_get.return_value = ret
+        assert fm_rating_service.get(1, 2) == ret
+        mock_get.assert_called_once_with(1, 2)
 
-    yield test_client
+def test_get_average_by_feature_model(fm_rating_service):
+    with patch('app.modules.featuremodel.services.FMRatingRepository.get_average_by_feature_model') as mock_get_avg:
+        ret = MagicMock()
+        mock_get_avg.return_value = ret
+        assert fm_rating_service.get_average_by_feature_model(1) == ret
+        mock_get_avg.assert_called_once_with(1)
 
-
-def test_sample_assertion(test_client):
-    """
-    Sample test to verify that the test framework and environment are working correctly.
-    It does not communicate with the Flask application; it only performs a simple assertion to
-    confirm that the tests in this module can be executed.
-    """
-    greeting = "Hello, World!"
-    assert greeting == "Hello, World!", "The greeting does not coincide with 'Hello, World!'"
+def test_create_or_update(fm_rating_service):
+    with patch('app.modules.featuremodel.services.FMRatingRepository.create_or_update') as mock_create_or_update:
+        ret = MagicMock()
+        mock_create_or_update.return_value = ret
+        assert fm_rating_service.create_or_update(1, 2, 3) == ret
+        mock_create_or_update.assert_called_once_with(1, 2, 3)
