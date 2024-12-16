2895d3454861982db2f02d1d66300f45f42b6bb7
Raúl Heras <rauherper@alum.us.es>
2024-12-16 22:54:04 +0100

    fix(model): fix incorrect count of models. #245

--- app/modules/featuremodel/repositories.py
+++ app/modules/featuremodel/repositories.py
@@ -5,18 +5,20 @@ from core.repositories.BaseRepository import BaseRepository
 from typing import Optional
 from app.modules.featuremodel.models import FMRating
 
+
 class FeatureModelRepository(BaseRepository):
     def __init__(self):
         super().__init__(FeatureModel)
 
     def count_feature_models(self) -> int:
-        max_id = self.model.query.with_entities(func.max(self.model.id)).scalar()
-        return max_id if max_id is not None else 0
+        count = self.model.query.with_entities(func.count(self.model.id)).scalar()
+        return count if count is not None else 0
 
 
 class FMMetaDataRepository(BaseRepository):
     def __init__(self):
         super().__init__(FMMetaData)
+
 
 class FMRatingRepository(BaseRepository):
     def __init__(self):
@@ -26,7 +28,8 @@ class FMRatingRepository(BaseRepository):
         return self.model.query.filter_by(feature_model_id=feature_model_id, user_id=user_id).first()
 
     def get_average_by_feature_model(self, feature_model_id: int) -> float:
-        return self.model.query.filter_by(feature_model_id=feature_model_id).with_entities(func.avg(self.model.rating)).scalar()
+        avg = self.model.query.filter_by(feature_model_id=feature_model_id).with_entities(func.avg(self.model.rating))
+        return avg.scalar()
 
     def create_or_update(self, feature_model_id: int, user_id: int, rating: int) -> FMRating:
         existing_rating = self.get(feature_model_id, user_id)
